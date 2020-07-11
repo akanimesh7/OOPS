664177f5de4ecdd9f6289ac4e55962b5c2210c12
ChangedGlobal <akanimesh7@gmail.com>
2020-07-11 21:23:36 +0530

    Update Gear class: 	Added rimDiameter,tyreDiameter to 	incorporate behaviour of changing wheels 	Changed constructor !!!

--- src/Bicycle/Gear.java
+++ src/Bicycle/Gear.java
@@ -3,13 +3,29 @@ package Bicycle;
 public class Gear {
 	private double chainRing;
 	private double cog;
+	private double rimDiameter;
+	private double tyreDiameter;
 
-	public Gear(double chainRing, double cog) {
+	public Gear(double chainRing, double cog,double rimDiameter, double tyreDiameter) {
 		this.chainRing = chainRing;
 		this.cog = cog;
+		this.rimDiameter = rimDiameter;
+		this.tyreDiameter = tyreDiameter;
+	}
+	private double wheelDiameter() {
+		return rimDiameter + 2*tyreDiameter;
 	}
 
-	double getRatio() {
-		return Double.isInfinite(chainRing / cog)?Double.POSITIVE_INFINITY:(chainRing/cog);
+	private double gearRatio() {
+		return Double.isInfinite(chainRing / cog) ? Double.POSITIVE_INFINITY : (chainRing / cog);
 	}
+	
+	public double gearInches() {
+		return wheelDiameter()*gearRatio();
+	}
+	
+//	public static void main(String[] args) {
+//		Gear g = new Gear(1,2,3,4);
+//		System.out.println(g.gearInches());
+//	}
 }
