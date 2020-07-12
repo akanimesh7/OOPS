bc7dfb7cce80e53363d28c4dcc24bf9813eab148
ChangedGlobal <akanimesh7@gmail.com>
2020-07-12 12:00:06 +0530

    Created a seperate class Wheel 	Removed multiple wheels functionality 	Wheel is a single wheel

--- src/Bicycle/Gear.java
+++ src/Bicycle/Gear.java
@@ -3,18 +3,9 @@ package Bicycle;
 import java.util.Arrays;
 
 public class Gear {
-	private double chainRing; // Getters and setters needed
+	private double chainRing;
 	private double cog;
-	private double rimDiameter;
-	private double tyreDiameter;
-	final static int DEAFULT = 1;
-	final static int RIM = 0;
-	final static int TYRE = 1;
-	private double wheels[][];
 //	Getters and setters
-	public double[][] getWheels() {
-		return wheels;
-	}
 
 	public double getChainRing() {
 		return chainRing;
@@ -32,83 +23,24 @@ public class Gear {
 		this.cog = cog;
 	}
 
-	public void setWheels(double[][] wheels) {
-		this.wheels = wheels;
-	}
-
-	public double getRimDiameter() {
-		return rimDiameter;
-	}
-	
-	public double getRimDiameter(double wheel[]) {
-		return wheel[RIM];
-	}
-
-	public void setRimDiameter(double rimDiameter) {
-		this.rimDiameter = rimDiameter;
-	}
-
-	public double getTyreDiameter() {
-		return tyreDiameter;
-	}
-	
-	public double getTyreDiameter(double wheel[]) {
-		return wheel[TYRE];
-	}
-
-	public void setTyreDiameter(double tyreDiameter) {
-		this.tyreDiameter = tyreDiameter;
-	}
-	
 //	Behaviour Functions
-	
-	private double wheelDiameter(double wheel[]) {
-		return  (getRimDiameter(wheel))+ (2 * getTyreDiameter(wheel));
-	}
-	
-	private double wheelDiameter() {
-		return getRimDiameter() + 2 * getTyreDiameter();
-	}
 
 	private double gearRatio() {
 		return Double.isInfinite(getChainRing() / getCog()) ? Double.POSITIVE_INFINITY : (getChainRing() / getCog());
 	}
 
-	public double gearInches() {
-		return wheelDiameter() * gearRatio();
+	public double gearInches(Wheel wheel) {
+		return wheel.wheelDiameter() * gearRatio();
 	}
 
-	private double[] getDiameters() {
-		double wheelRef[][] = getWheels();
-		int noOfEntries = wheelRef.length;
-		double diameters[] = new double[noOfEntries];
-		for (int i = 0; i < noOfEntries; i++) {
-			diameters[i] = wheelDiameter(wheelRef[i]);
-		}
-		return diameters;
-	}
-	
-	// Constructor
-	public Gear(double[][] wheels) {
-		// rearrange input to match your requirements
-		// if tyre is first column then swap columns here
-		setChainRing(DEAFULT);
-		setCog(DEAFULT);
-		setRimDiameter(DEAFULT);
-		setTyreDiameter(DEAFULT);
-		setWheels(wheels);
-	}
-
-	public Gear(double chainRing, double cog, double rimDiameter, double tyreDiameter) {
+	public Gear(double chainRing, double cog) {
 		setChainRing(chainRing);
 		setCog(cog);
-		setRimDiameter(rimDiameter);
-		setTyreDiameter(tyreDiameter);
 	}
-
-//	public static void main(String[] args) {
-//		double arr[][] = {{1,2},{3,4}};
-//		Gear g = new Gear(arr);
-//		System.out.println(Arrays.toString(g.getDiameters()));
-//	}
+	
+	public static void main(String[] args) {
+		Gear g = new Gear(1,2);
+		Wheel w = new Wheel(3, 4);
+		System.out.println((g.gearInches(w)));
+	}
 }
---
+++ src/Bicycle/Wheel.java
@@ -0,0 +1,91 @@
+package Bicycle;
+
+public class Wheel {
+	private double rimDiameter;
+	private double tyreDiameter;
+	final static int DEAFULT = 1;
+	final static int RIM = 0;
+	final static int TYRE = 1;
+
+	public double getRimDiameter() {
+		return rimDiameter;
+	}
+	
+	public Wheel() {
+		setRimDiameter(DEAFULT);
+		setTyreDiameter(DEAFULT);
+	}
+
+	public Wheel(double rimDiameter, double tyreDiameter) {
+		setRimDiameter(rimDiameter);
+		setTyreDiameter(tyreDiameter);
+	}
+
+//	public double getRimDiameter(double wheel[]) {
+//		return wheel[RIM];
+//	}
+
+	public void setRimDiameter(double rimDiameter) {
+		this.rimDiameter = rimDiameter;
+	}
+
+	public double getTyreDiameter() {
+		return tyreDiameter;
+	}
+
+//	public double getTyreDiameter(double wheel[]) {
+//		return wheel[TYRE];
+//	}
+
+	public void setTyreDiameter(double tyreDiameter) {
+		this.tyreDiameter = tyreDiameter;
+	}
+
+	public double wheelDiameter() {
+		return (getRimDiameter()) + (2 * getTyreDiameter());
+	}
+
+	private double getCircumference() {
+		return Math.PI * wheelDiameter();
+	}
+
+}
+
+
+//private double wheels[][];
+
+//public Wheel(double[][] wheels) {
+//	setWheels(wheels);
+//}
+//
+//public double[][] getWheels() {
+//	return wheels;
+//}
+//
+//public void setWheels(double[][] wheels) {
+//	this.wheels = wheels;
+//}
+
+//private double[] getCircumferences() {
+//double wheelRef[][] = getWheels();
+//int noOfEntries = wheelRef.length;
+//double circumferences[] = new double[noOfEntries];
+//for(int i=0;i<noOfEntries;i++) {
+//	circumferences[i] = Math.PI * wheelDiameter(wheelRef[i]);
+//}
+//return circumferences;
+//}
+
+//private double wheelDiameter() {
+//return getRimDiameter() + 2 * getTyreDiameter();
+//}
+
+//private double[] getDiameters() {
+//double wheelRef[][] = getWheels();
+//int noOfEntries = wheelRef.length;
+//double diameters[] = new double[noOfEntries];
+//for (int i = 0; i < noOfEntries; i++) {
+//	diameters[i] = wheelDiameter(wheelRef[i]);
+//}
+//return diameters;
+//}
